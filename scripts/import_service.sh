1b32129ceb0b24d55869649b173f680579e1b46f
Sergio Gutierrez <sgutierr@localhost.localdomain>
2018-02-06 14:54:27 +0100

    cli scripts

--- scripts/import_service.sh
+++ scripts/import_service.sh
@@ -3,18 +3,19 @@
 3scale-cli import swagger -f /cicd/swaggers/payment_swagger.json -p "{method}{path}" -m true -s $SERVICE_ID
 3scale-cli activedocs create --systemName PaymentsAPI  -f /cicd/swaggers/payment_swagger.json
 
-// Application plan Basic
-3scale-cli app-plan create --service $SERVICE_ID --plan Basic
-3scale-cli metrics list -s $SERVICE_ID
+aplication_plan_basic_id= $(3scale-cli app-plan create --service $SERVICE_ID --plan Basic | awk '{ print $10; }')
+aplication_plan_unlimited_id= $(3scale-cli app-plan create --service $SERVICE_ID --plan Unlimited | awk '{ print $10; }')
 
-3scale-cli limits create  --service $SERVICE_ID --appplan 13 --metric 13 --period day --unit 0
-3scale-cli limits create  --service $SERVICE_ID --appplan 13 --metric 14 --period week --unit 0
-3scale-cli limits create  --service $SERVICE_ID --appplan 13 --metric 15 --period eternity --unit 0
-3scale-cli limits create  --service $SERVICE_ID --appplan 13 --metric 1$SERVICE_ID --period eternity --unit 0
-3scale-cli app-plan create --service $SERVICE_ID --plan unlimited
+metric_id = $(3scale-cli metrics list -s 8 | awk '{ print $1;}' | while read line && [ -z "$id" ]; do [[ ! "$line" =~ ^[0-9]+$ ]] || id=$line echo $line; done | head -n 1 )
+metric_unlimited_id= $metric_id
+
+3scale-cli limits create  --service $SERVICE_ID --appplan $aplication_plan_basic_id --metric $metric_id --period day --unit 0
+3scale-cli limits create  --service $SERVICE_ID --appplan $aplication_plan_basic_id --metric $((metric_id+1)) --period week --unit 0
+3scale-cli limits create  --service $SERVICE_ID --appplan $aplication_plan_basic_id --metric $((metric_id+1)) --period eternity --unit 0
+3scale-cli limits create  --service $SERVICE_ID --appplan $aplication_plan_basic_id --metric $((metric_id+1)) --period eternity --unit 0
 
 
-3scale-cli limits create  --service $SERVICE_ID --appplan 14 --metric 13 --period day --unit 0
-3scale-cli limits create  --service $SERVICE_ID --appplan 14 --metric 14 --period week --unit 0
-3scale-cli limits create  --service $SERVICE_ID --appplan 14 --metric 15 --period eternity --unit 0
-3scale-cli limits create  --service $SERVICE_ID --appplan 14 --metric 1$SERVICE_ID --period eternity --unit 0
+3scale-cli limits create  --service $SERVICE_ID --appplan $aplication_plan_unlimited_id --metric $metric_unlimited_id --period day --unit 0
+3scale-cli limits create  --service $SERVICE_ID --appplan $aplication_plan_unlimited_id --metric $((metric_unlimited_id+1)) --period week --unit 0
+3scale-cli limits create  --service $SERVICE_ID --appplan $aplication_plan_unlimited_id --metric $((metric_unlimited_id+1)) --period eternity --unit 0
+3scale-cli limits create  --service $SERVICE_ID --appplan $aplication_plan_unlimited_id --metric $((metric_unlimited_id+1)) --period eternity --unit 0
