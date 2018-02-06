257345e07892058463707da5543e25337330cc20
Sergio Gutierrez <sgutierr@localhost.localdomain>
2018-02-06 14:56:59 +0100

    import service

--- scripts/import_service.sh
+++ scripts/import_service.sh
@@ -6,7 +6,7 @@
 aplication_plan_basic_id= $(3scale-cli app-plan create --service $SERVICE_ID --plan Basic | awk '{ print $10; }')
 aplication_plan_unlimited_id= $(3scale-cli app-plan create --service $SERVICE_ID --plan Unlimited | awk '{ print $10; }')
 
-metric_id = $(3scale-cli metrics list -s 8 | awk '{ print $1;}' | while read line && [ -z "$id" ]; do [[ ! "$line" =~ ^[0-9]+$ ]] || id=$line echo $line; done | head -n 1 )
+metric_id = $(3scale-cli metrics list -s $SERVICE_ID | awk '{ print $1;}' | while read line && [ -z "$id" ]; do [[ ! "$line" =~ ^[0-9]+$ ]] || id=$line echo $line; done | head -n 1 )
 metric_unlimited_id= $metric_id
 
 3scale-cli limits create  --service $SERVICE_ID --appplan $aplication_plan_basic_id --metric $metric_id --period day --unit 0
