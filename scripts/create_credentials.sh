11e8473f27e11d4bd4af3a5e08ae2159935d293e
Sergio Gutierrez <sgutierr@localhost.localdomain>
2018-02-20 11:58:17 +0100

    change enviroment variable

--- scripts/create_credentials.sh
+++ scripts/create_credentials.sh
@@ -2,4 +2,4 @@
 
 # Create a new creedentials.json
 
-echo '{ "threescale": {  "id": "'$ID'", "access_token": "'$ACCESS_TOKEN'", "wildcard": "'$WILDCARD_DOMAIN'"  }}' >> $HOME/.3scale/credentials.json
+echo '{ "threescale": {  "id": "'$SUBDOMAIN'", "access_token": "'$ACCESS_TOKEN'", "wildcard": "'$WILDCARD_DOMAIN'"  }}' >> $HOME/.3scale/credentials.json
