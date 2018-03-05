f861a1dbc0dfaa67b5b08fb11f74aafdd24163a5
Sergio Gutierrez <sgutierr@localhost.localdomain>
2018-03-05 15:02:52 +0100

    bug fixing

--- scripts/create_credentials.sh
+++ scripts/create_credentials.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-
+SUBDOMAIN=$1
+ACCESS_TOKEN=$2
+WILDCARD_DOMAIN=$3
 # Create a new creedentials.json
 
 echo '{ "threescale": {  "id": "'$SUBDOMAIN'", "access_token": "'$ACCESS_TOKEN'", "wildcard": "'$WILDCARD_DOMAIN'"  }}' >> $HOME/.3scale/credentials.json
