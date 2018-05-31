ec88b1959384649cfa23ccff42b98475f606649d
Sergio Gutierrez <sgutierr@localhost.localdomain>
2018-05-31 11:44:27 +0200

    production version fix

--- scripts/promotion_to_production.sh
+++ scripts/promotion_to_production.sh
@@ -10,15 +10,15 @@ if [ -z "$2" ];
  lastVersion=$(3scale-cli proxy-configs list -s $SERVICE_ID -e sandbox | awk '{ print $2;}' | tail -n 2) 
 fi
 
-productionVersion=$(3scale-cli proxy-configs list -s $SERVICE_ID -e production | awk '{ print $2;}' | tail -n 2) 
+# productionVersion=$(3scale-cli proxy-configs list -s $SERVICE_ID -e production | awk '{ print $2;}' | tail -n 2) 
 
 # Get production version
-if [ $lastVersion \> $productionVersion ];
- then
- 3scale-cli proxy-configs promote -s $SERVICE_ID -e sandbox -c $lastVersion
- else
- echo Staging and production have the same version: $productionVersion
-fi
+#if [ $lastVersion \> $productionVersion ];
+# then
+3scale-cli proxy-configs promote -s $SERVICE_ID -e sandbox -c $lastVersion
+# else
+# echo Staging and production have the same version: $productionVersion
+# fi
 
 
 
