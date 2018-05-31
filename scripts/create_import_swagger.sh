fc340706961816fb008d8960bb28b718329a7b8e
Sergio Gutierrez <sgutierr@localhost.localdomain>
2018-05-31 10:32:35 +0200

    add apicast routes replacement

--- scripts/create_import_swagger.sh
+++ scripts/create_import_swagger.sh
@@ -32,3 +32,17 @@ fi
 3scale-cli activedocs create --systemName $SERVICE_NAME -f $SWG 
 # Create an application plan to test service status
 aplication_plan_basic_id=$(3scale-cli app-plan create --service $SERVICE_ID --plan Basic | awk '{ print $10; }')
+account_id=$(3scale-cli accounts list | awk '{ print $1;}' | while read line && [ -z "$id" ]; do [[ ! "$line" =~ ^[0-9]+$ ]] || id=$line echo $line; done | head -n 1 )
+plan_id=$(3scale-cli app-plan list -s $SERVICE_ID | awk '{ print $1;}' | while read line && [ -z "$id" ]; do [[ ! "$line" =~ ^[0-9]+$ ]] || id=$line echo $line; done | head -n 1)
+app_id=$(3scale-cli applications create --account $account_id --plan $plan_id --name app_to_remove --description test | grep " id:" | awk '{ print $2;}')
+
+api_test_path=$(3scale-cli proxy show -s $SERVICE_ID | grep api_test_path | awk '{ print $2;}')
+sanbox_endpoint=$(3scale-cli proxy show -s $SERVICE_ID | grep sandbox_endpoint: | awk '{ print $2;}')
+sanbox_endpoint=echo "$sanbox_endpoint" | tr '[_]' '-'
+backend_endpoint=$(3scale-cli proxy show -s $SERVICE_ID | grep api_backend: | awk '{ print $2;}' | head -n 1)
+production_endpoint=$(3scale-cli proxy show -s $SERVICE_ID | grep endpoint: | awk '{ print $2;}' | head -n 1)
+production_endpoint=echo "$production_endpoint" | tr '[_]' '-'
+
+3scale-cli proxy update -s $SERVICE_ID -S $sanbox_endpoint -e $production_endpoint -b $backend_endpoint
+
+
