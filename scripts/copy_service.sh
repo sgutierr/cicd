14d573f03e17bd8f71f5e371545cab7fc5441c30
Sergio Gutierrez <sgutierr@localhost.localdomain>
2018-06-26 13:19:31 +0200

    automate service id

--- scripts/copy_service.sh
+++ scripts/copy_service.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-ID1=$1
+SERVICE_NAME=$1
 SRC=$2
 DEST=$3
 # Copy service
+ID=$(3scale-cli services list | grep $SERVICE_NAME | awk '{ print $1 }')
 3scale-copy service $ID1 -t "$(date "+%Y%m%d%H%M")" --source $SRC --destination $DEST
