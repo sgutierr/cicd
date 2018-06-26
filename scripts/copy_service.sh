a74091d23a2b6e195df6833fb8e9fdb70b21789b
Sergio Gutierrez <sgutierr@localhost.localdomain>
2018-06-26 13:13:04 +0200

    auto system name

--- scripts/copy_service.sh
+++ scripts/copy_service.sh
@@ -2,6 +2,5 @@
 ID1=$1
 SRC=$2
 DEST=$3
-NAME=$4
 # Copy service
-3scale-copy service $ID1 -t $NAME --source $SRC --destination $DEST
+3scale-copy service $ID1 -t "$(date "+%Y%m%d%H%M")" --source $SRC --destination $DEST
