1c7e092a936530515782c3fad3dc73c03bd692de
sgutierr <sgutierr@localhost.localdomain>
2019-02-22 11:21:00 +0100

    remove service_name parameter

--- scripts/copy_service.sh
+++ scripts/copy_service.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
-SERVICE_NAME=$1
+SWG=$1
 SRC=$2
 DEST=$3
-SWG=$4
 
-if [ -z "$4" ];
-then
-  SERVICE_ID=$(3scale-cli services list | grep $SERVICE_NAME | awk '{ print $1 }')
-# parameter swagger path exists we find out service_name
-else
   # get version field
   VERS=$(more $SWG | grep version | cut -d '"' -f4 | head -1)
   # get title field
@@ -20,5 +14,10 @@ else
   echo Service name:$SERVICE_NAME
   SERVICE_ID=$(3scale-cli services list | grep $BASE_NAME-v$MAJOR | awk '{ print $1 }')
   echo $SERVICE_ID
-fi
+
+if [ -z "$SERVICE_ID" ];
+then
+echo "SERVICE_ID doesn't found"
+else
 3scale-copy service $SERVICE_ID -t "$(date "+%Y%m%d%H%M")" --source $SRC --destination $DEST
+fi
--- scripts/promotion_to_production.sh
+++ scripts/promotion_to_production.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
-SERVICE_NAME=$1
-SWG=$2
-lastVersion=$3
+SWG=$1
+lastVersion=$2
 
-
-if [ -z "$2" ];
-then
-  SERVICE_ID=$(3scale-cli services list | grep $SERVICE_NAME | awk '{ print $1 }')
-# parameter swagger path exists we find out service_name
-else
   # get version field
   VERS=$(more $SWG | grep version | cut -d '"' -f4 | head -1)
   # get title field
@@ -20,23 +13,15 @@ else
   echo Service name:$SERVICE_NAME
   SERVICE_ID=$(3scale-cli services list | grep $BASE_NAME-v$MAJOR | awk '{ print $1 }')
   echo $SERVICE_ID
-fi
 
-# There is not version specified 
-if [ -z "$3" ];
+# There is not version specified
+if [ -z "$lastVersion" ];
  then
- lastVersion=$(3scale-cli proxy-configs list -s $SERVICE_ID -e sandbox | awk '{ print $2;}' | tail -n 2) 
+ lastVersion=$(3scale-cli proxy-configs list -s $SERVICE_ID -e sandbox | awk '{ print $2;}' | tail -n 2)
 fi
 
-# productionVersion=$(3scale-cli proxy-configs list -s $SERVICE_ID -e production | awk '{ print $2;}' | tail -n 2) 
-
-# Get production version
-#if [ $lastVersion \> $productionVersion ];
-# then
 3scale-cli proxy-configs promote -s $SERVICE_ID -e sandbox -c $lastVersion
-# else
-# echo Staging and production have the same version: $productionVersion
-# fi
+
 
 
 
